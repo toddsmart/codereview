6f41535b51140785832070852b173e967bd37c44
todd <todd.smart@smartbinary.com>
2015-09-02 09:12:02 -0400

    updates to demonstrate the use of a native javascript based MVC pattern

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,15 +1,80 @@
-var model = (function () {
-    var privateVar,
-        model;
+$(document).ready(function () {
+    /**
+     * things we'd put into a model
+     */
+    var model = (function () {
+        var privateVar;
 
-    model = {
-        getPrivateVar : function () {
-            return privateVar;
-        },
-        setPrivateVar : function (value) {
-            privateVar = value;
-        }
-    };
+        return {
+            get: function () {
+                return privateVar;
+            },
+            set: function (value) {
+                privateVar = value;
+            }
+        };
+    }());
 
-    return model;
-} ());
+    /**
+     * things we'd put into the view
+     */
+    var view = (function () {
+        var javaScriptDiv,
+            jqueryDiv;
+
+        javaScriptDiv = document.getElementById('javaScriptDiv');
+
+        jqueryDiv = $('#jqueryDiv');
+
+        return {
+            javaScriptDiv: javaScriptDiv,
+            jqueryDiv: jqueryDiv
+        };
+    }());
+
+    /**
+     * things we'd put into a controller
+     */
+    var  controller = (function () {
+        var updateJavaScriptDiv,
+            updateJqueryDiv;
+
+        updateJavaScriptDiv = function (text) {
+            /*simple dom manipulation w/js */
+            view.javaScriptDiv.innerText = text + model.get();
+        };
+
+        updateJqueryDiv = function (text) {
+            /*simple dom manipulation w/jquery */
+            view.jqueryDiv.text(text + model.get());
+        };
+
+        return {
+            updateJavaScriptDiv: updateJavaScriptDiv,
+            updateJqueryDiv: updateJqueryDiv
+        };
+    }());
+
+
+
+
+    /**
+     *
+     * Things an "app" might do
+     *
+     */
+    // use session storage
+    sessionStorage.setItem('testItem', 10);
+    document.getElementById('sessionStorageTest').innerText = sessionStorage.getItem('testItem');
+    var appVar = Number(sessionStorage.getItem('testItem'));
+
+    // use closure to protect private var
+    model.set(appVar + 1);
+    document.getElementById('privateVarDiv').innerText = model.get();
+
+    // use a controller that uses the model to update the view
+    model.set(model.get() + 1);
+    controller.updateJavaScriptDiv('This is content for the javascript div - model value is:');
+    model.set(model.get() + 1);
+    controller.updateJqueryDiv('This is content for the javascript div - model value is:');
+});
