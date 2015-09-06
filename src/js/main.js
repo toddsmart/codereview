3d09196a44e5eae1a291b2ef89336f4e243cadbc
todd <todd.smart@smartbinary.com>
2015-09-05 21:16:54 -0400

    added some labwork from http://eloquentjavascript.net/02_program_structure.html

--- src/js/main.js
+++ src/js/main.js
@@ -106,22 +106,79 @@ $(document).ready(function () {
     //    }
     //
     //Answer: 5
-    //
-    //
-    //Question: write a palindrone function
-    //
-    //Answer:
-    //var palindrone = function (text) {
-    //    var firstHalf,
-    //        secondHalf,
-    //        halfLength = text.length / 2;
-    //
-    //    firstHalf = text.substr(0, halfLength).split('');
-    //    secondHalf = text.substr(halfLength).split('');
-    //
-    //    return firstHalf.reverse() === secondHalf;
-    //};
-    //
-    //console.log(palindrone('asdffdsa'));
+
+    /**
+     * Question: write a palindrone function
+     */
+    var palindrone = function (text) {
+        var firstHalf,
+            secondHalf,
+            halfLength = text.length / 2;
+
+        firstHalf = text.substr(0, halfLength).split('');
+        secondHalf = text.substr(halfLength).split('');
+
+        return firstHalf.reverse() === secondHalf;
+    };
+
+    /**
+     * Write a loop that makes seven calls to console.log to output the following triangle:
+     #
+     ##
+     ###
+     ####
+     #####
+     ######
+     #######
+     */
+    var logTreePounds = function () {
+        for (var pounds = '#'; pounds < '########'; pounds += '#') {
+            console.log(pounds);
+        }
+    }
+
+    /**
+     * Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers
+     * divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+     * When you have that working, modify your program to print "FizzBuzz", for numbers that are divisible by both
+     * 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+     */
+    var logFizzBuzz = function () {
+        var fizzBuzz = '';
+        for (var i = 1; i < 101; i++) {
+            if (i % 3 === 0 && i % 5 === 0) {
+                console.log('FizzBuzz');
+            } else if (i % 3 === 0 && i % 5 !== 0) {
+                console.log('Fizz');
+            } else if (i % 5 === 0 && i % 3 !== 0) {
+                console.log('Buzz');
+            } else {
+                console.log(i);
+            }
+        }
+    }
+
+    /**
+     * Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines.
+     * At each position of the grid there is either a space or a “#” character. The characters should form a chess board.
+     *
+     */
+    var logCheckerBoard = function () {
+        var size = 8,
+            fullString = '',
+            rowString = '';
+        for (var y = 0; y < size; y++) {
+            for (var x = 0; x < size; x++) {
+                if (y % 2) {
+                    rowString += '# ';
+                } else {
+                    rowString += ' #';
+                }
+            }
+            fullString += (rowString + '\n');
+            rowString = '';
+        }
+        console.log(fullString);
+    }
 
 });
